ee334c2d150f3a58fb22a1795aa77c6535e7c5b6
abhay5624 <abhaysharma7435@gmail.com>
2024-04-07 17:04:15 +0530

    api added

--- frontend/src/Components/Results.jsx
+++ frontend/src/Components/Results.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import "../Css/Results.css"
 import Resultlink from './Resultlink.jsx'
+import {getJson} from 'serpapi'
 export default function Results() {
 
     const fetchImages = () =>{
 
     }
-    const fetchLinks = () =>{
-        
+    const fetchLinks = async () =>{
+        const url = 'https://real-time-web-search.p.rapidapi.com/search?q=apple%20an%20online%20store&limit=100';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6485a298ccmsh50c49557d180251p144e64jsn5cd0c942dacd',
+		'X-RapidAPI-Host': 'real-time-web-search.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
     }
     return (
 
