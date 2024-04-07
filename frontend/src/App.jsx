1c165ddf6e75debf4d7fb17f09df4e99bfb58bf0
priyanshu kumar <dlnpriyanshu@gmail.com>
2024-04-07 15:18:56 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+function App() {
+
+  return (
+    <>
+    
+      <h1 className='text-red-700'>Pixel-Sync round 2</h1>
+
+ 
+   
+    </>
+  )
+}
+
+export default App
